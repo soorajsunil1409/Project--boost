54c452bbf8572e4cf077391263c4de3b6340e55e
sooraj1409 <sooraj.namboothiry@gmail.com>
2020-07-01 20:57:52 +0530

    Project Boost v2 (Rocket Thrust Function)

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating Right");
+        }
+    }
+}
