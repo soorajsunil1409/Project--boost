cedae601a5278633137b3599c222277728b93d94
sooraj1409 <sooraj.namboothiry@gmail.com>
2020-07-02 21:13:53 +0530

    Project_Boost v7 (Using Scene Management and adding time delay b/w loading of scene)

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,15 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class Rocket : MonoBehaviour
 {
+    // todo fix lighting bug
 
     [SerializeField] float mainThrust = 100f;
     [SerializeField] float rcsThrust = 200f;
 
     Rigidbody rigidbody;
     AudioSource audioSource;
+
+    enum State { Alive, Dying, Transcending };
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start()
@@ -21,24 +24,42 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
     }
+
 
     void OnCollisionEnter (Collision collision)
     {
+        if (state != State.Alive) { return; }
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                print("OK");
                 break;
             case "Finish":
                 print("You Won!");
+                Invoke("LoadNextScene", 2f);
                 break;
             default:
-                print("Dead");
+                print("Hit something deadly");
+                state = State.Dying;
+                Invoke("LoadFirstLevel", 2f);
                 break;
         }
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1); // todo allow for more than 2 levels
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
     }
 
     private void Thrust()
